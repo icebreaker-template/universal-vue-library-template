dcf4307e0dc5ed7b3ab560c858f9cd7757b8d351
SonOfMagic <qq1324318532@gmail.com>
2022-02-28 16:25:05 +0800

    feat: remove icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`
+      entry: path.resolve(__dirname, "src/components/HelloWorld.vue"),
+      name: "MyLib",
+      fileName: (format) => `my-lib.${format}.js`,
+      // formats:['umd','cjs','es']
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ["vue"],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
+          vue: "Vue",
+        },
+      },
+    },
   },
   optimizeDeps: {
-    exclude: ['vue-demi']
-  }
-})
+    exclude: ["vue-demi"],
+  },
+});
