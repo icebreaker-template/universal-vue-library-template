f7ad5b773c05086959313802e98bb3477f620970
SonOfMagic <qq1324318532@gmail.com>
2022-03-01 14:56:31 +0800

    feat: commit codes

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld/src/index.vue";
+import HelloWorld2 from "vue3-hello-world";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld2 msg="Hello HelloWorld2" />
 </template>
 
 <style>
